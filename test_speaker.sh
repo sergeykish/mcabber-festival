78b79a927e857ff834d99ccc5940573ba214f770
Sergey Kish <sergey.kish@gmail.com>
2009-05-03 13:31:39 +0300

    more semantic filenames

--- test_speaker.sh
+++ test_speaker.sh
@@ -2,6 +2,6 @@
 
 BODY="$HOME/.mcabber/event_files/mcabber-10628.sVg5yU"
 echo 'hello!' >"$BODY"
-~/.mcabber/event-handler.py MSG IN checker@gmail.com $BODY
+~/.mcabber/speaker.py MSG IN checker@gmail.com $BODY
 echo 'again )' >"$BODY"
-~/.mcabber/event-handler.py MSG IN checker@gmail.com $BODY
+~/.mcabber/speaker.py MSG IN checker@gmail.com $BODY
